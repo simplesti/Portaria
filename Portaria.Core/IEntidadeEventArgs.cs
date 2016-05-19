ac85dae17c2328bf98753debe6781f9120d07889
simplesti <simplesti.tecnologia@gmail.com>
2016-05-18 22:33:45 -0300

    Removendo projetos não relacionados ao PD Desktop.

---
+++ Portaria.Core/IEntidadeEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using Portaria.Core.Model;
+using System;
+
+namespace Portaria.Core
+{
+    public class IEntidadeEventArgs : EventArgs
+    {
+        public IEntidade Entidade { get; set; }
+
+        public IEntidadeEventArgs(IEntidade entidade) : base()
+        {
+            this.Entidade = entidade;
+        }
+    }
+}
